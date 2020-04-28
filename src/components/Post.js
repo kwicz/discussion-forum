3571d6d9a8a8d2c19cf823d7eb3aec283191562d
Katy Solovewicz <katy@solovewicz.com>
2020-04-28 13:46:41 -0700

    add post fragment, and event handlers in post control
    Co-authored-by: Zakkrey <zakkreyjshort@gmail.com>
    Co-authored-by: kristinaht <hengster.tintor@gmail.com>

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,11 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Post(props){
-
+  return (
+    <React.Fragment>
+      <div onClick = {() => props.whenPostClicked(props.id)}>
+        <h3>{props.title}</h3>
+        <p>by {props.username}</p>
+        <p>{props.body}</p>
+        <button onClick = {()=> props.whenUpvoteClicked(props.id)} type="submit">Upvote</button>
+        <button onClick = {()=> props.whenDownvoteClicked(props.id)} type="submit">Downvote</button>
+      </div>
+    </React.Fragment>
+  )
 }
 
 Post.propTypes = {
-
+  username: PropTypes.string,
+  title: PropTypes.string,
+  body: PropTypes.string,
+  date: PropTypes.string,
+  upvotes: PropTypes.number,
+  downvotes: PropTypes.number,
+  id: PropTypes.string,
+  whenPostClicked: PropTypes.func,
+  whenUpvoteClicked: PropTypes.func,
+  whenDownvoteClicked: PropTypes.func
 }
 
 export default Post;
--- src/components/PostControl.js
+++ src/components/PostControl.js
@@ -1,6 +1,55 @@
+import React from 'react';
+import NewPostForm from '';
+import PostList from '';
+import PostDetails from '';
+import EditPostForm from '';
 
+class PostControl extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedPost: null,
+      editing: false
+    };
+  }
+
+  handleClick = () => {
+    if (this.state.selectedPost !== null) {
+      this.setState({
+        selectedPost: null,
+        editing: false
+      });
+    } else {
+      const { dispatch } = this.props;
+      const action = {
+        type: 'TOGGLE_FORM'
+      }
+      dispatch(action);
+    }
+  }
+
+  handleEditClick = () => {
+    this.setState({editing: true});
+  }
+
+}
 
 handleAddingNewPostToList = (newPost) => {
-    const ( dispatch ) = this.props;
-    const { id, name, title, date, upvotes, downvotes } = newPost;
+  const ( dispatch ) = this.props;
+  const { id, username, title, date, upvotes, downvotes } = newPost;
+  const action = {
+    type: 'ADD_ITEM',
+    id: id,
+    username: username,
+		title: title,
+		date: date,
+		upvotes: upvotes,
+		downvotes: downvotes,
+	}
+	dispatch(action);
+	const action2 = {
+		type: 'TOGGLE_FORM'
+	}
+	dispatch(action2);
 }
